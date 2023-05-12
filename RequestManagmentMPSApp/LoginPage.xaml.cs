027a569e29a700e0eca744f31c6d45f63f8fe793
дима gfhbkjd <2329192@mail.ru>
2023-05-12 23:25:24 +0700

    1

--- RequestManagmentMPSApp/LoginPage.xaml.cs
+++ RequestManagmentMPSApp/LoginPage.xaml.cs
@@ -24,5 +24,10 @@ namespace RequestManagmentMPSApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
